1a673bae384833795cf4505b2eaee2acbca78973
EmmaNiu9403 <Emma9403@126.com>
2016-07-10 16:26:58 -0500

    CHANGED
    CHANGED

--- Main.java
+++ Main.java
@@ -11,94 +11,133 @@
  * Fall 2015
  */
 
-
 package assignment3;
+
 import java.util.*;
 import java.io.*;
 import java.lang.reflect.Array;
 
-
-
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner kb = new Scanner(System.in);
-		
+
 		// TODO methods to read in words, output ladder
 		String start = new String();
 		String end = new String();
 		System.out.println("please enter start and end words");
 		String temp = kb.nextLine();
-		String [] words = temp.split(" ");
-		if(words.length == 2){
-		start = words[0];
-		end = words[1];
+		String[] words = temp.split(" ");
+		if (words.length == 2) {
+			start = words[0];
+			end = words[1];
 		}
-		ArrayList <String > ladder = getWordLadderBFS(start, end);
+		ArrayList<String> ladder = getWordLadderBFS(start, end);
 		System.out.println("word ladder is: ");
-		for(String s: ladder){
+		for (String s : ladder) {
 			System.out.println(s);
-			
+
 		}
 		kb.close();
 
 	}
-	
+
 	public static ArrayList<String> getWordLadderDFS(String start, String end) {
-		
+
 		// TODO some code
 		Set<String> dict = makeDictionary();
 		// TODO more code
-		
+
 		return null; // replace this line later with real return
 	}
-	
-    public static ArrayList<String> getWordLadderBFS(String start, String end) {
-		
+
+	public static ArrayList<String> getWordLadderBFS(String start, String end) {
+
 		// TODO some code
-    	LinkedList <WordNode> quene = new LinkedList <WordNode>();
-    	ArrayList <String> WordLadder=new ArrayList <String>();
 		Set<String> dict = makeDictionary();
-		if(!dict.contains(start)) throw new IllegalArgumentException("Error: illegal Input");
-		if(!dict.contains(end)) throw new IllegalArgumentException("Error: illegal Input");
-		// TODO more code
-		quene.add(new WordNode(start));
-		WordLadder.add(start);
-		while(!quene.isEmpty()){
-			WordNode word = quene.remove();
-			if(word.GetString().equals(end)){
-			WordLadder.add(word.GetString());
-				return WordLadder;
+		ArrayList <ArrayList<String>> result = new ArrayList <ArrayList<String>> ();
+
+		LinkedList<WordNode> queue = new LinkedList<WordNode>();
+		queue.add(new WordNode(start, 1, null));
+
+		dict.add(end);
+
+		int minStep = 0;
+
+		HashSet<String> visited = new HashSet<String>();
+		HashSet<String> unvisited = new HashSet<String>();
+		unvisited.addAll(dict);
+
+		int preNumSteps = 0;
+
+		while (!queue.isEmpty()) {
+			WordNode top = queue.remove();
+			String word = top.word;
+			int currNumSteps = top.numSteps;
+
+			if (word.equals(end)) {
+				if (minStep == 0) {
+					minStep = top.numSteps;
 				}
-			if(word.GetFlag()==true);
-			else{
-				word.SetFlag();
-				WordLadder.add(word.GetString());
-				char [] arr = word.GetString().toCharArray();
-				for(int i=0;i<arr.length;i++){
-					char temp = arr[i];
-					for(char c='A';c<='Z';c++){
-						if(arr[i]!=c) arr[i]=c;
-						String newWord = new String(arr);
-						if(dict.contains(newWord)) quene.add(new WordNode(newWord));
-						
+
+				if (top.numSteps == minStep && minStep != 0) {
+					// nothing
+					ArrayList<String> t = new ArrayList<String>();
+					t.add(top.word);
+					while (top.pre != null) {
+						t.add(0, top.pre.word);
+						top = top.pre;
 					}
-					arr[i]=temp;
+					result.add(t);
+					continue;
+				}
+
+			}
+
+			if (preNumSteps < currNumSteps) {
+				unvisited.removeAll(visited);
+			}
+
+			preNumSteps = currNumSteps;
+
+			char[] arr = word.toCharArray();
+			for (int i = 0; i < arr.length; i++) {
+				for (char c = 'a'; c <= 'z'; c++) {
+					char temp = arr[i];
+					if (arr[i] != c) {
+						arr[i] = c;
+					}
+
+					String newWord = new String(arr);
+					if (unvisited.contains(newWord)) {
+						queue.add(new WordNode(newWord, top.numSteps + 1, top));
+						visited.add(newWord);
+					}
+
+					arr[i] = temp;
 				}
 			}
+
 		}
-		
-		
-		
-		return null; // replace this line later with real return
+		ArrayList <String> temp = new ArrayList <String> ();
+		int min =0;
+        for(int i=0;i<result.size();i++){
+        	if(result.get(i).size()<result.get(min).size()){
+        		min=i;
+        	}
+        	
+        }
+        temp = result.get(min);
+		return temp;
 	}
-    
-	public static Set<String>  makeDictionary () {
+
+
+	public static Set<String> makeDictionary() {
 		Set<String> words = new HashSet<String>();
 		Scanner infile = null;
 		try {
-			infile = new Scanner (new File("five_letter_words.txt"));
+			infile = new Scanner(new File("five_letter_words.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("Dictionary File not Found!");
 			e.printStackTrace();
--- WordNode.java
+++ WordNode.java
@@ -1,27 +1,14 @@
 package assignment3;
 
 public class WordNode {
-	boolean visitRecord;
-	String word;
-
-	public WordNode() {
-
-	}
-
-	public WordNode(String word) {
-		this.word = word;
-	}
-
-	public void SetFlag(){
-		visitRecord=true;
-	}
-
-	public String GetString() {
-		return word;
-
-	}
 	
-	public boolean GetFlag(){
-		return visitRecord;
-	}
+	    String word;
+	    int numSteps;
+	    WordNode pre;
+	 
+	    public WordNode(String word, int numSteps, WordNode pre){
+	        this.word = word;
+	        this.numSteps = numSteps;
+	        this.pre = pre;
+	    }
 }
