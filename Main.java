989eccc81c505baf7a24218c3610ab4cff6d5a47
KassPerez <Kassandra.perez@utexas.edu>
2016-07-10 17:31:59 -0500

    updated print method and added test case

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 /* WORD LADDER Main.java
  * EE422C Project 3 submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
+ * Kassandra Perez
+ * Kap2589
  * <Student1 5-digit Unique No.>
  * <Student2 Name>
  * <Student2 EID>
@@ -11,140 +11,143 @@
  * Fall 2015
  */
 
-package assignment3;
 
+package assignment3;
 import java.util.*;
 import java.io.*;
-import java.lang.reflect.Array;
 
 public class Main {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+	{
 		Scanner kb = new Scanner(System.in);
-
-		// TODO methods to read in words, output ladder
-		String start = new String();
-		String end = new String();
-		System.out.println("please enter start and end words");
-		String temp = kb.nextLine();
-		String[] words = temp.split(" ");
-		if (words.length == 2) {
-			start = words[0];
-			end = words[1];
-		}
-		ArrayList<String> ladder = getWordLadderBFS(start, end);
-		System.out.println("word ladder is: ");
-		if (ladder == null)
-			System.out.println("null");
-		else {
-			for (String s : ladder) {
-				System.out.println(s);
-
-			}
-		}
+		String tempAnswer = kb.nextLine();
+		ArrayList<String> words = getWords(tempAnswer.trim().split(" "));
+		
+		// TODO Create a loop to keep asking for words
+		// TODO Create a method that checks if the word is a command and if its a valid command
+		
+		ArrayList<String> wordLadder1 = getWordLadderBFS(words.get(0).toUpperCase(), words.get(1).toUpperCase());
+//		ArrayList<String> wordLadder1 = getWordLadderDFS(words.get(0).toUpperCase(),words.get(1).toUpperCase()); 
+		
+		printWordLadder(words.get(0).toUpperCase(), words.get(1).toUpperCase(),wordLadder1);
+		
 		kb.close();
-
 	}
-
-	public static ArrayList<String> getWordLadderDFS(String start, String end) {
-
-		// TODO some code
-		Set<String> dict = makeDictionary();
-		// TODO more code
-
-		return null; // replace this line later with real return
-	}
-
-	public static ArrayList<String> getWordLadderBFS(String start, String end) {
-
-		// TODO some code
-		Set<String> dict = makeDictionary();
-		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
-
-		LinkedList<WordNode> queue = new LinkedList<WordNode>();
-		queue.add(new WordNode(start, 1, null));
-
-		dict.add(end);
-
-		int minStep = 0;
-
-		HashSet<String> visited = new HashSet<String>();
-		HashSet<String> unvisited = new HashSet<String>();
-		unvisited.addAll(dict);
-
-		int preNumSteps = 0;
-
-		while (!queue.isEmpty()) {
-			WordNode top = queue.remove();
-			String word = top.word;
-			int currNumSteps = top.numSteps;
-
-			if (word.equals(end)) {
-				if (minStep == 0) {
-					minStep = top.numSteps;
-				}
-
-				if (top.numSteps == minStep && minStep != 0) {
-					// nothing
-					ArrayList<String> t = new ArrayList<String>();
-					t.add(top.word);
-					while (top.pre != null) {
-						t.add(0, top.pre.word);
-						top = top.pre;
-					}
-					result.add(t);
-					continue;
-				}
-
-			}
-
-			if (preNumSteps < currNumSteps) {
-				unvisited.removeAll(visited);
-			}
-
-			preNumSteps = currNumSteps;
-
-			char[] arr = word.toCharArray();
-			for (int i = 0; i < arr.length; i++) {
-				for (char c = 'A'; c <= 'Z'; c++) {
-					char temp = arr[i];
-					if (arr[i] != c) {
-						arr[i] = c;
-					}
-
-					String newWord = new String(arr);
-					if (unvisited.contains(newWord)) {
-						queue.add(new WordNode(newWord, top.numSteps + 1, top));
-						visited.add(newWord);
-					}
-
-					arr[i] = temp;
-				}
-			}
-
+	
+	/**
+	 * @param start
+	 * @param end
+	 * @return
+	 */
+	public static ArrayList<String> getWordLadderDFS(String start, String end) 
+	{
+		
+		Set<String> dict = makeDictionary(); 
+		
+		if(dict.contains(start) && dict.contains(end))
+		{
+			ArrayList<String> wordLadder = new ArrayList<String>();
+			wordLadder.add(start);
+			DFS dfs = new DFS(wordLadder);
+			wordLadder = dfs.createWordLadder(end, dict, -1);
+			
+			return wordLadder;
 		}
-		ArrayList<String> temp = new ArrayList<String>();
-		int min = 0;
-		for (int i = 0; i < result.size(); i++) {
-			if (result.get(i).size() < result.get(min).size()) {
-				min = i;
-			}
-
-		}
-		if (result != null) {
-			temp = result.get(min);
-			return temp;
-		} else
-			return null;
+		// TODO ask if i should have the error handling here or in main
+		return null; 
+		
 	}
+	
+    public static ArrayList<String> getWordLadderBFS(String start, String end)
+    {
+    	Set<String> dict = makeDictionary();
+    	ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
 
-	public static Set<String> makeDictionary() {
+    	LinkedList<WordNode> queue = new LinkedList<WordNode>();
+    	queue.add(new WordNode(start, 1, null));
+
+    	dict.add(end);
+
+    	int minStep = 0;
+
+    	HashSet<String> visited = new HashSet<String>();
+    	HashSet<String> unvisited = new HashSet<String>();
+    	unvisited.addAll(dict);
+
+    	int preNumSteps = 0;
+
+    	while (!queue.isEmpty()) {
+    		WordNode top = queue.remove();
+    		String word = top.word;
+    		int currNumSteps = top.numSteps;
+
+    		if (word.equals(end)) {
+    			if (minStep == 0) {
+    				minStep = top.numSteps;
+    			}
+
+    			if (top.numSteps == minStep && minStep != 0) {
+    				// nothing
+    				ArrayList<String> t = new ArrayList<String>();
+    				t.add(top.word);
+    				while (top.pre != null) {
+    					t.add(0, top.pre.word);
+    					top = top.pre;
+    				}
+    				result.add(t);
+    				continue;
+    			}
+
+    		}
+
+    		if (preNumSteps < currNumSteps) {
+    			unvisited.removeAll(visited);
+    		}
+
+    		preNumSteps = currNumSteps;
+
+    		char[] arr = word.toCharArray();
+    		for (int i = 0; i < arr.length; i++) {
+    			for (char c = 'A'; c <= 'Z'; c++) {
+    				char temp = arr[i];
+    				if (arr[i] != c) {
+    					arr[i] = c;
+    				}
+
+    				String newWord = new String(arr);
+    				if (unvisited.contains(newWord)) {
+    					queue.add(new WordNode(newWord, top.numSteps + 1, top));
+    					visited.add(newWord);
+    				}
+
+    				arr[i] = temp;
+    			}
+    		}
+
+    	}
+    	ArrayList<String> temp = new ArrayList<String>();
+    	int min = 0;
+    	for (int i = 0; i < result.size(); i++) {
+    		if (result.get(i).size() < result.get(min).size()) {
+    			min = i;
+    		}
+
+    	}
+    	if (!result.isEmpty()) {
+    		temp = result.get(min);
+    		return temp;
+    	} else
+    		return null;
+    }
+
+	public static Set<String> makeDictionary ()
+	{
 		Set<String> words = new HashSet<String>();
 		Scanner infile = null;
 		try {
-			// infile = new Scanner(new File("five_letter_words.txt"));
-			infile = new Scanner(new File("short_dict.txt"));
+			//infile = new Scanner (new File("short_dict.txt"));
+			infile = new Scanner (new File("five_letter_words.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("Dictionary File not Found!");
 			e.printStackTrace();
@@ -155,4 +158,36 @@ public class Main {
 		}
 		return words;
 	}
+	
+	public static ArrayList<String> getWords(String[] words)
+	{
+		ArrayList<String> newWord = new ArrayList<String>();
+		for(String s: words)
+		{
+			if(!(s.trim().isEmpty()))
+			{
+				newWord.add(s);
+			}
+		}
+		return newWord;
+	}
+	
+	public static void printWordLadder(String start, String end, ArrayList<String> wordLadder)
+	{
+		if(wordLadder ==  null)
+		{
+			System.out.println("No word ladder found!");
+		}
+		else
+		{
+			System.out.println("a " + (wordLadder.size() - 2)+ "-rung word ladder exists between " + start + " and " + end +".");
+
+			for(String s: wordLadder)
+			{
+				System.out.println(s);
+			}
+		}
+		
+	}
+	
 }
--- TestCase.java
+++ TestCase.java
@@ -4,11 +4,108 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestCase {
+public class TestCase
+{
+
+	//*************************** No Duplicates in Word Ladder Test *******************************//
 
 	@Test
-	public void test() {
+	public void testDuplicatesBFS() 
+	{
+		// TODO Map to keep take of the words map<string, int>
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDuplicatesDFS() 
+	{
+		// TODO Map to keep take of the words map<string, int>
+		fail("Not yet implemented");
+	}
+	
+	//*************************** Same Letter Position Changed Test *******************************//
+
+	@Test
+	public void testSamePosChangeBFS() 
+	{
+		// TODO create a compare method that records the index of the changed letter position
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSamePosChangeDFS() 
+	{
+		// TODO create a compare method that records the index of the changed letter position
+		fail("Not yet implemented");
+	}
+	
+	private int compareLetterPos(String pre, String next)
+	{
+		for(int i = 0; i < pre.length(); i++)
+		{
+			if(pre.charAt(i) != next.charAt(i))
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	//*************************** No Word Ladder Test *******************************//
+	@Test
+	public void testNoWordLadderBFS() 
+	{
+		// TODO what words do not have a word ladder
+		// Use Short_dictionary to test
+		assertEquals(null,Main.getWordLadderBFS(null, null));
+	}
+	
+	@Test
+	public void testNoWordLadderDFS() 
+	{
+		// TODO what words do not have a word ladder
+		assertEquals(null,Main.getWordLadderDFS(null, null));
+	}
+
+	//*************************** Normal Word Ladder Test *******************************//
+
+	@Test
+	public void testBFS() 
+	{
+		// TODO 
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDFS() 
+	{
+		// TODO 
+		fail("Not yet implemented");
+	}
+	
+	//*************************** Illegal Input Test *******************************//
+	
+	@Test
+	public void testIllegalInputWordsNotInDict() 
+	{
+		// TODO run main code and see if it prints what it needs to print
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testIllegalInputIllegalCommand() 
+	{
+		// TODO run main code and see if it prints what it needs to print
 		fail("Not yet implemented");
 	}
 
+	//*************************** Illegal Input Test *******************************//
+	
+	@Test
+	public void testQuitCommand() 
+	{
+		// TODO 
+		fail("Not yet implemented");
+	}
+	
 }
