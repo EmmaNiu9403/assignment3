58ae1fd02d9822d66f23bcd7edeae913616fa05c
EmmaNiu9403 <Emma9403@126.com>
2016-07-11 17:49:51 -0500

    Updated test cases
    updated test cases

--- TestCase.java
+++ TestCase.java
@@ -2,7 +2,8 @@ package assignment3;
 
 import static org.junit.Assert.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import org.junit.Test;
 
@@ -14,65 +15,13 @@ public class TestCase
 	@Test
 	public void testDuplicatesBFS() 
 	{
-		String start = (new String("Stone")).toUpperCase();
-		String end = (new String("Money")).toUpperCase();
-		ArrayList<String> wordLadder = Main.getWordLadderBFS(start, end);		
-		Main.printWordLadder(start, end, wordLadder);
-		boolean duplicates = isDuplicateWordInLadder(wordLadder);
-		System.out.println("Does this wordLadder have duplicates? " + duplicates);
-		assertEquals(false,duplicates);
+		// TODO Map to keep take of the words map<string, int>
 	}
 	
 	@Test
 	public void testDuplicatesDFS() 
 	{
-		String start = (new String("Stone")).toUpperCase();
-		String end = (new String("Money")).toUpperCase();
-		ArrayList<String> wordLadder = Main.getWordLadderDFS(start, end);
-		Main.printWordLadder(start, end, wordLadder);
-		boolean duplicates = isDuplicateWordInLadder(wordLadder);
-		System.out.println("Does this wordLadder have duplicates? " + duplicates);
-		assertEquals(false,duplicates);
-	}
-	
-	@Test
-	public void testDuplicateTestingMethod()
-	{
-		ArrayList<String> wordLadder = new ArrayList<String>();
-		wordLadder.add("Heart");
-		wordLadder.add("Hears");
-		wordLadder.add("Heirs");
-		wordLadder.add("Hears");
-		wordLadder.add("Bears");
-		Main.printWordLadder("Heart","Bears", wordLadder);
-		System.out.println("Does this wordLadder have duplicates? " + isDuplicateWordInLadder(wordLadder));
-		assertEquals(true,isDuplicateWordInLadder(wordLadder));
-	}
-	
-	private boolean isDuplicateWordInLadder(ArrayList<String> wordLadder)
-	{
-		Map<String,Integer> duplicates = new HashMap<String,Integer>();
-		for(String s: wordLadder)
-		{
-			if(duplicates.containsKey(s))
-			{
-				duplicates.put(s, (duplicates.get(s).intValue()) + 1);
-			}
-			else
-			{
-				duplicates.put(s,1); 
-			}
-		}
-		
-		Set<String> keys = duplicates.keySet();
-		for(String key: keys)
-		{
-			if(duplicates.get(key).intValue() > 1)
-			{
-				return true;
-			}
-		}
-		return false;
+		// TODO Map to keep take of the words map<string, int>
 	}
 	
 	//*************************** Same Letter Position Changed Test *******************************//
@@ -80,62 +29,37 @@ public class TestCase
 	@Test
 	public void testSamePosChangeBFS() 
 	{
-		String start = (new String("Stone")).toUpperCase();
-		String end = (new String("Money")).toUpperCase();
-		ArrayList<String> wordLadder = Main.getWordLadderBFS(start, end);		
-		Main.printWordLadder(start, end, wordLadder);
-		boolean sameLetterPosChanged = isLetterPosChanged(wordLadder);
-		System.out.println("Does this wordLadder have duplicates? " + sameLetterPosChanged);
-		assertEquals(false,sameLetterPosChanged);
+		// TODO create a compare method that records the index of the changed letter position
+		System.out.println("Please enter Start and End Words");
+		Scanner kb = new Scanner(System.in);
+		String temp = new String();
+		temp = kb.nextLine();
+		String[] t = temp.split(" ");
+		String start = new String(t[0]);
+		String end = new String(t[1]);
+		ArrayList <String> test = new ArrayList <String> (Main.getWordLadderBFS(start, end));
+		for(int i=0;i<test.size()-2;i++){
+			if(compareLetterPos(test.get(i),test.get(i+1))==compareLetterPos(test.get(i+1),test.get(i+2))) 
+			System.out.println("failed test cases");
+		}
+		
 	}
 	
 	@Test
 	public void testSamePosChangeDFS() 
 	{
-		String start = (new String("Stone")).toUpperCase();
-		String end = (new String("Money")).toUpperCase();
-		ArrayList<String> wordLadder = Main.getWordLadderBFS(start, end);		
-		Main.printWordLadder(start, end, wordLadder);
-		boolean sameLetterPosChanged = isLetterPosChanged(wordLadder);
-		System.out.println("Does this wordLadder have duplicates? " + sameLetterPosChanged);
-		assertEquals(false,sameLetterPosChanged);
-	}
-	
-	@Test
-	public void testSameLetterPosChangeTestingMethod()
-	{
-		ArrayList<String> wordLadder = new ArrayList<String>();
-		wordLadder.add("Heart");
-		wordLadder.add("Hears");
-		wordLadder.add("Heirs");
-		wordLadder.add("Heers");
-		wordLadder.add("Bears");
-		Main.printWordLadder("Heart","Bears", wordLadder);
-		boolean sameLetterPosChanged = isLetterPosChanged(wordLadder);
-		System.out.println("Does this wordLadder have duplicates? " + sameLetterPosChanged);
-		assertEquals(true,sameLetterPosChanged);
-	}
-	
-	private boolean isLetterPosChanged(ArrayList<String> wordLadder)
-	{
-		if(wordLadder == null)
-		{
-			return false;
+		System.out.println("Please enter Start and End Words");
+		Scanner kb = new Scanner(System.in);
+		String temp = new String();
+		temp = kb.nextLine();
+		String[] t = temp.split(" ");
+		String start = new String(t[0]);
+		String end = new String(t[1]);
+		ArrayList <String> test = new ArrayList <String> (Main.getWordLadderDFS(start, end));
+		for(int i=0;i<test.size()-2;i++){
+			if(compareLetterPos(test.get(i),test.get(i+1))==compareLetterPos(test.get(i+1),test.get(i+2))) 
+			System.out.println("failed test cases");
 		}
-		
-		int pos = -1;
-		String word = wordLadder.get(0);
-		for(int i = 1; i < wordLadder.size(); i++)
-		{
-			int tempPos = compareLetterPos(word, wordLadder.get(i));
-			if(pos == tempPos)
-			{
-				return true;
-			}
-			pos = tempPos;
-			word = wordLadder.get(i);
-		}
-		return false;
 	}
 	
 	private int compareLetterPos(String pre, String next)
@@ -151,7 +75,7 @@ public class TestCase
 	}
 
 	//*************************** No Word Ladder Test *******************************//
-	//@Test
+	@Test
 	public void testNoWordLadderBFS() 
 	{
 		// TODO what words do not have a word ladder
@@ -159,7 +83,7 @@ public class TestCase
 		assertEquals(null,Main.getWordLadderBFS(null, null));
 	}
 	
-	//@Test
+	@Test
 	public void testNoWordLadderDFS() 
 	{
 		// TODO what words do not have a word ladder
@@ -172,14 +96,12 @@ public class TestCase
 	public void testBFS() 
 	{
 		// TODO 
-		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void testDFS() 
 	{
 		// TODO 
-		fail("Not yet implemented");
 	}
 	
 	//*************************** Illegal Input Test *******************************//
@@ -188,23 +110,14 @@ public class TestCase
 	public void testIllegalInputWordsNotInDict() 
 	{
 		// TODO run main code and see if it prints what it needs to print
-		fail("Not yet implemented");
+		String start = "APPLES";
+		String end = "HELLO";
+		assertEquals(null, Main.getWordLadderDFS(start, end));
+		
 	}
 	
-	@Test
-	public void testIllegalInputIllegalCommand() 
-	{
-		// TODO run main code and see if it prints what it needs to print
-		fail("Not yet implemented");
-	}
 
-	//*************************** /quit Command Test *******************************//
+	//*************************** Illegal Input Test *******************************//
 	
-	@Test
-	public void testQuitCommand() 
-	{
-		// TODO 
-		fail("Not yet implemented");
-	}
 	
 }
