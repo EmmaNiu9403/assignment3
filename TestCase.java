4adc8267f35ce934b7f0d9d43b84995a5063cdfd
KassPerez <Kassandra.perez@utexas.edu>
2016-07-11 17:08:19 -0500

    duplicates and same letter pos test cases updated

--- TestCase.java
+++ TestCase.java
@@ -2,6 +2,8 @@ package assignment3;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
+
 import org.junit.Test;
 
 public class TestCase
@@ -12,15 +14,65 @@ public class TestCase
 	@Test
 	public void testDuplicatesBFS() 
 	{
-		// TODO Map to keep take of the words map<string, int>
-		fail("Not yet implemented");
+		String start = (new String("Stone")).toUpperCase();
+		String end = (new String("Money")).toUpperCase();
+		ArrayList<String> wordLadder = Main.getWordLadderBFS(start, end);		
+		Main.printWordLadder(start, end, wordLadder);
+		boolean duplicates = isDuplicateWordInLadder(wordLadder);
+		System.out.println("Does this wordLadder have duplicates? " + duplicates);
+		assertEquals(false,duplicates);
 	}
 	
 	@Test
 	public void testDuplicatesDFS() 
 	{
-		// TODO Map to keep take of the words map<string, int>
-		fail("Not yet implemented");
+		String start = (new String("Stone")).toUpperCase();
+		String end = (new String("Money")).toUpperCase();
+		ArrayList<String> wordLadder = Main.getWordLadderDFS(start, end);
+		Main.printWordLadder(start, end, wordLadder);
+		boolean duplicates = isDuplicateWordInLadder(wordLadder);
+		System.out.println("Does this wordLadder have duplicates? " + duplicates);
+		assertEquals(false,duplicates);
+	}
+	
+	@Test
+	public void testDuplicateTestingMethod()
+	{
+		ArrayList<String> wordLadder = new ArrayList<String>();
+		wordLadder.add("Heart");
+		wordLadder.add("Hears");
+		wordLadder.add("Heirs");
+		wordLadder.add("Hears");
+		wordLadder.add("Bears");
+		Main.printWordLadder("Heart","Bears", wordLadder);
+		System.out.println("Does this wordLadder have duplicates? " + isDuplicateWordInLadder(wordLadder));
+		assertEquals(true,isDuplicateWordInLadder(wordLadder));
+	}
+	
+	private boolean isDuplicateWordInLadder(ArrayList<String> wordLadder)
+	{
+		Map<String,Integer> duplicates = new HashMap<String,Integer>();
+		for(String s: wordLadder)
+		{
+			if(duplicates.containsKey(s))
+			{
+				duplicates.put(s, (duplicates.get(s).intValue()) + 1);
+			}
+			else
+			{
+				duplicates.put(s,1); 
+			}
+		}
+		
+		Set<String> keys = duplicates.keySet();
+		for(String key: keys)
+		{
+			if(duplicates.get(key).intValue() > 1)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	//*************************** Same Letter Position Changed Test *******************************//
@@ -28,15 +80,62 @@ public class TestCase
 	@Test
 	public void testSamePosChangeBFS() 
 	{
-		// TODO create a compare method that records the index of the changed letter position
-		fail("Not yet implemented");
+		String start = (new String("Stone")).toUpperCase();
+		String end = (new String("Money")).toUpperCase();
+		ArrayList<String> wordLadder = Main.getWordLadderBFS(start, end);		
+		Main.printWordLadder(start, end, wordLadder);
+		boolean sameLetterPosChanged = isLetterPosChanged(wordLadder);
+		System.out.println("Does this wordLadder have duplicates? " + sameLetterPosChanged);
+		assertEquals(false,sameLetterPosChanged);
 	}
 	
 	@Test
 	public void testSamePosChangeDFS() 
 	{
-		// TODO create a compare method that records the index of the changed letter position
-		fail("Not yet implemented");
+		String start = (new String("Stone")).toUpperCase();
+		String end = (new String("Money")).toUpperCase();
+		ArrayList<String> wordLadder = Main.getWordLadderBFS(start, end);		
+		Main.printWordLadder(start, end, wordLadder);
+		boolean sameLetterPosChanged = isLetterPosChanged(wordLadder);
+		System.out.println("Does this wordLadder have duplicates? " + sameLetterPosChanged);
+		assertEquals(false,sameLetterPosChanged);
+	}
+	
+	@Test
+	public void testSameLetterPosChangeTestingMethod()
+	{
+		ArrayList<String> wordLadder = new ArrayList<String>();
+		wordLadder.add("Heart");
+		wordLadder.add("Hears");
+		wordLadder.add("Heirs");
+		wordLadder.add("Heers");
+		wordLadder.add("Bears");
+		Main.printWordLadder("Heart","Bears", wordLadder);
+		boolean sameLetterPosChanged = isLetterPosChanged(wordLadder);
+		System.out.println("Does this wordLadder have duplicates? " + sameLetterPosChanged);
+		assertEquals(true,sameLetterPosChanged);
+	}
+	
+	private boolean isLetterPosChanged(ArrayList<String> wordLadder)
+	{
+		if(wordLadder == null)
+		{
+			return false;
+		}
+		
+		int pos = -1;
+		String word = wordLadder.get(0);
+		for(int i = 1; i < wordLadder.size(); i++)
+		{
+			int tempPos = compareLetterPos(word, wordLadder.get(i));
+			if(pos == tempPos)
+			{
+				return true;
+			}
+			pos = tempPos;
+			word = wordLadder.get(i);
+		}
+		return false;
 	}
 	
 	private int compareLetterPos(String pre, String next)
@@ -52,7 +151,7 @@ public class TestCase
 	}
 
 	//*************************** No Word Ladder Test *******************************//
-	@Test
+	//@Test
 	public void testNoWordLadderBFS() 
 	{
 		// TODO what words do not have a word ladder
@@ -60,7 +159,7 @@ public class TestCase
 		assertEquals(null,Main.getWordLadderBFS(null, null));
 	}
 	
-	@Test
+	//@Test
 	public void testNoWordLadderDFS() 
 	{
 		// TODO what words do not have a word ladder
@@ -99,7 +198,7 @@ public class TestCase
 		fail("Not yet implemented");
 	}
 
-	//*************************** Illegal Input Test *******************************//
+	//*************************** /quit Command Test *******************************//
 	
 	@Test
 	public void testQuitCommand() 
