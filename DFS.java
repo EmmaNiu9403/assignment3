425c9934b5c7896f4f20ef4ff479fba5b9be09ab
KassPerez <Kassandra.perez@utexas.edu>
2016-07-11 19:47:50 -0500

    fixed DFS method
    seems to have gotten rid of the stackoverflow problem

--- DFS.java
+++ DFS.java
@@ -5,14 +5,12 @@ import java.util.*;
 public class DFS 
 {	
 	private ArrayList<String> wordLadder;
-	private Set<String> visited;
 	
 	public DFS(){}
 	
-	public DFS(ArrayList<String> wordLadder)
+	public DFS(ArrayList<String> wordLadder,String startWord)
 	{
 		this.wordLadder = wordLadder;
-		visited = new HashSet<String>();
 	}
 	
 	public ArrayList<String> createWordLadder(String end, Set<String> dict, int pos)
@@ -24,7 +22,26 @@ public class DFS
 		}
 		
 		int changedLetter = -1;
-		//Set<Node> possibleWords = new HashSet<Node>();
+		for(int i = 0; i < lastWord.length(); i++)
+		{
+			char[] tempStr = lastWord.toCharArray();
+			if(i != pos && tempStr[i] != end.charAt(i))
+			{
+				tempStr[i] = end.charAt(i);
+				String newWord = new String(tempStr);
+				if(dict.contains(newWord) && !wordLadder.contains(newWord))
+				{
+					changedLetter = i;
+					wordLadder.add(newWord);
+					createWordLadder(end,dict,changedLetter);
+					if(wordLadder.get(wordLadder.size()-1).equals(end))
+					{
+						return wordLadder;
+					}
+					wordLadder.remove(wordLadder.size()-1);
+				}
+			}
+		}
 		for(int i = 0; i < lastWord.length() ;i++)
 		{
 			char[] tempStr = lastWord.toCharArray();
@@ -32,7 +49,7 @@ public class DFS
 			{
 				for(char c = 'A'; c <= 'Z'; c++)
 				{
-					if(tempStr[i] != c)
+					if(tempStr[i] != c && c != end.charAt(i))
 					{
 						tempStr[i] = c;
 						String newWord = new String(tempStr);
@@ -54,40 +71,7 @@ public class DFS
 			
 		}
 		
-//		for(String word: dict)
-//		{
-//			if(isDiffOne(word,lastWord) && !wordLadder.contains(word))
-//			{
-//				changedLetter = letterPosChanged(word,lastWord);
-//				if(changedLetter != pos)
-//				{
-//					//possibleWords.add(new Node(word,letterMatchWithEnd(word,end)));
-//					wordLadder.add(word);
-//					createWordLadder(end,dict,changedLetter);
-//					if(wordLadder.get(wordLadder.size()-1).equals(end))
-//					{
-//						return wordLadder;
-//					}
-//					wordLadder.remove(wordLadder.size()-1);
-//				}
-//			}
-//		}
-		
-//		for(Node cand: possibleWords)
-//		{
-//			wordLadder.add(cand.getWord());
-//			createWordLadder(end,dict,changedLetter);
-//			if(wordLadder.get(wordLadder.size()-1).equals(end))
-//			{
-//				return wordLadder;
-//			}
-//			wordLadder.remove(wordLadder.size()-1);
-//		}
-//		
 		return null;
 	}
-	
-
-
 }
 
